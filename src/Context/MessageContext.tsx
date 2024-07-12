a168e8113268bff126217020956d6bfaaf2871db
Rinat Canaan <58044154+rcanaan@users.noreply.github.com>
2024-07-12 12:34:54 +0300

    using uuid as unique id

--- src/Context/MessageContext.tsx
+++ src/Context/MessageContext.tsx
@@ -1,42 +1,51 @@
 // src/context/MessageContext.tsx
 import React, { createContext, useState, ReactNode, useContext } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-interface Message {
-  id: number;
+type MessageType = "sent" | "received";
+
+export interface MessageDetails {
+  id: string;
   text: string;
   timestamp: number;
-  type: "sent" | "received";
+  // timestamp: Date;
+  type: MessageType;
 }
 
 interface MessageContextType {
-  messages: Message[];
-  addMessage: (text: string, type: "sent" | "received") => void;
-  resendMessage: (id: number) => void;
-  deleteMessage: (id: number) => void;
+  messages: MessageDetails[];
+  addMessage: (text: string, type: MessageType) => void;
+  resendMessage: (id: string) => void;
+  deleteMessage: (id: string) => void;
 }
-
-const MessageContext = createContext<MessageContextType | undefined>(undefined);
+const emptyState: MessageContextType = {
+  messages: [],
+  addMessage: () => {},
+  resendMessage: () => {},
+  deleteMessage: () => {},
+};
+const MessageContext = createContext<MessageContextType>(emptyState);
 
 export const MessageProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  const [messages, setMessages] = useState<Message[]>([]);
+  const [messages, setMessages] = useState<MessageDetails[]>([]);
 
-  const addMessage = (text: string, type: "sent" | "received") => {
-    const newMessage = { id: Date.now(), text, timestamp: Date.now(), type };
+  const addMessage = (text: string, type: MessageType) => {
+    const newMessage = { id: uuidv4(), text, timestamp: Date.now(), type };
     setMessages((prevMessages) =>
       [...prevMessages, newMessage].sort((a, b) => b.timestamp - a.timestamp)
     );
   };
 
-  const resendMessage = (id: number) => {
+  const resendMessage = (id: string) => {
     const message = messages.find((msg) => msg.id === id);
     if (message) {
       addMessage(message.text, message.type);
     }
   };
 
-  const deleteMessage = (id: number) => {
+  const deleteMessage = (id: string) => {
     setMessages((prevMessages) => prevMessages.filter((msg) => msg.id !== id));
   };
 
